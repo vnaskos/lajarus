2ea5079002a1c3e8293c05a6266e56643406e2db
lenikbt <milenazhuleku@gmail.com>
2016-11-09 12:42:17 +0200

    Create basic player controller

---
+++ src/main/java/com/bugbusters/lajarus/controller/PlayerController.java
@@ -0,0 +1,36 @@
+package com.bugbusters.lajarus.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bugbusters.lajarus.entity.PlayerEntity;
+import com.bugbusters.lajarus.model.Player;
+import com.bugbusters.lajarus.service.PlayerService;
+
+@RestController
+@RequestMapping(value = "/player")
+public class PlayerController {
+	
+	private PlayerService playerService;
+	
+	@Autowired
+	public PlayerController(PlayerService playerService) {
+		this.playerService = playerService;
+	}
+
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public List<PlayerEntity> getAll() {
+		return playerService.getAll();
+	}
+	
+	@RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
+	public Player findPlayerByName(@PathVariable String name) throws Exception {
+		return playerService.findPlayerByName(name);
+	}
+	
+}
---
+++ src/test/java/com/bugbusters/lajarus/controller/PlayerControllerTests.java
@@ -0,0 +1,42 @@
+package com.bugbusters.lajarus.controller;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bugbusters.lajarus.entity.PlayerEntity;
+import com.bugbusters.lajarus.model.Player;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PlayerControllerTests {
+
+	@Autowired
+	PlayerController playerController;
+	
+	@Test
+	public void createPlayerControllerTest() {
+		assertNotNull(playerController);
+	}
+
+	@Test
+	public void getAllPlayersTest() {
+		List<PlayerEntity> players = playerController.getAll();
+		assertEquals(players.size(), 2);
+	}
+	
+	@Test
+	public void findPlayerByNameAdminTest() throws Exception {
+		Player p = playerController.findPlayerByName("admin");
+		assertNotNull(p);
+		assertEquals(p.getName(), "admin");
+	}
+	
+}
