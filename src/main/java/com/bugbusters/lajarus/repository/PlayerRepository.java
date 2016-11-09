a1d64c90e787db38d626e331d29646dd87310de6
alexavge <alexavgerinou@hotmail.com>
2016-11-09 12:40:04 +0200

    Create player repository

---
+++ src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
@@ -0,0 +1,16 @@
+package com.bugbusters.lajarus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.bugbusters.lajarus.entity.PlayerEntity;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<PlayerEntity, Long>{
+	
+	@Query("SELECT p FROM PlayerEntity p WHERE p.name = :name")
+	public PlayerEntity findPlayerByName(@Param("name") String name);
+	
+}
