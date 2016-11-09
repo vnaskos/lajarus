bba965de09217ba8ef6df28d89875bed95a0e811
DreamerVisioner <mitrousis@outlook.com.gr>
2016-11-09 12:31:16 +0200

    Create Player entity

---
+++ src/main/java/com/bugbusters/lajarus/entity/PlayerEntity.java
@@ -0,0 +1,58 @@
+package com.bugbusters.lajarus.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "player")
+public class PlayerEntity {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	private double latitude;
+	private double longitude;
+	
+	public PlayerEntity() {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+}
