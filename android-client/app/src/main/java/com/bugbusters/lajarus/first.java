8992d956cc8ef6bb91555d210f0936827d579519
DreamerVisioner <mitrousis@outlook.com.gr>
2016-11-10 00:11:52 +0200

    android client Livadoxwritis vision :p

---
+++ android-client/app/src/main/java/com/bugbusters/lajarus/first.java
@@ -0,0 +1,51 @@
+package com.bugbusters.lajarus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class first extends AppCompatActivity {
+
+   public Button but1;
+
+    public void init(){
+        but1 = (Button)findViewById(R.id.button6);
+        but1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent toy = new Intent(first.this,second.class);
+                startActivity(toy);
+            }
+        });
+    }
+
+    public void init2(){
+        but1 = (Button)findViewById(R.id.button7);
+        but1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent toy2 = new Intent(first.this,third.class);
+                startActivity(toy2);
+            }
+        });
+    }
+
+    public void init3(){
+        but1 = (Button)findViewById(R.id.button8);
+        but1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent toy3 = new Intent(first.this,fourth.class);
+                startActivity(toy3);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        init();
+        init2();
+        init3();
+    }
+}
---
+++ android-client/app/src/main/java/com/bugbusters/lajarus/third.java
@@ -0,0 +1,13 @@
+package com.bugbusters.lajarus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class third extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
