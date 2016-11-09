8ade4258ff13e39f27571215dbf033f13ca1bf2f
Vasilis Naskos <vnaskos@gmail.com>
2016-11-09 12:19:40 +0200

    Initial import

---
+++ src/main/java/com/bugbusters/lajarus/LajarusApplication.java
@@ -0,0 +1,12 @@
+package com.bugbusters.lajarus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LajarusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LajarusApplication.class, args);
+	}
+}
