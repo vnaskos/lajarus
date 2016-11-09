76c843443b07e70a33802989abd3796bc03da010
thanasismavridis <thanasis_mavridis@yahoo.gr>
2016-11-09 12:34:39 +0200

    Create Player model and tests

---
+++ src/main/java/com/bugbusters/lajarus/model/Player.java
@@ -0,0 +1,52 @@
+package com.bugbusters.lajarus.model;
+
+public class Player {
+
+	private String name;
+	private double latitude;
+	private double longitude;
+	
+	public Player(String name, double latitude, double longitude) {
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(name)
+				.append(" (")
+				.append(latitude)
+				.append(",")
+				.append(longitude)
+				.append(")");
+		
+		return builder.toString();
+	}
+	
+}
---
+++ src/test/java/com/bugbusters/lajarus/model/PlayerTests.java
@@ -0,0 +1,48 @@
+package com.bugbusters.lajarus.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTests {
+
+	@Test
+	public void createPlayerTest() {
+		Player p = new Player("testPlayer", 10.3, 22.4);
+		assertNotNull(p);
+	}
+	
+	@Test
+	public void setPlayerNameTest() {
+		Player p = new Player("testPlayer", 10.3, 22.4);
+		assertEquals(p.getName(), "testPlayer");
+		
+		p.setName("testUser");
+		assertEquals(p.getName(), "testUser");
+	}
+	
+	@Test
+	public void setPlayerLatitudeTest() {
+		Player p = new Player("testPlayer", 10.3, 22.4);
+		assertEquals(p.getLatitude(), 10.3, 0.001);
+		
+		p.setLatitude(20.3);
+		assertEquals(p.getLatitude(), 20.3, 0.001);
+	}
+	
+	@Test
+	public void setPlayerLongitudeTest() {
+		Player p = new Player("testPlayer", 10.3, 22.4);
+		assertEquals(p.getLongitude(), 22.4, 0.001);
+		
+		p.setLongitude(20.3);
+		assertEquals(p.getLongitude(), 20.3, 0.001);
+	}
+	
+	@Test
+	public void playerToStringTest() {
+		Player p = new Player("testPlayer", 10.3, 22.4);
+		assertEquals(p.toString(), "testPlayer (10.3,22.4)");
+	}
+
+}
