1a766fae80935ed5f6241805942a05e5a22faed1
Karen <Karen@Karen>
2016-11-12 18:57:27 +0200

    PlayerController Modify : 	I added a new method for getting a request and showing the nearest players to the current player.
    Player Service :
    	I added two methods on PlayerService for getting the players
    and finding the players who are near to the current player.
    
    Player Entity:
    	I added a new characteristic id about to describe the Player.

--- server/src/main/java/com/bugbusters/lajarus/service/PlayerService.java
+++ server/src/main/java/com/bugbusters/lajarus/service/PlayerService.java
@@ -40,12 +40,14 @@ public class PlayerService {
                  List<PlayerEntity> playersEntity = playerRepository.findAll();
                  PlayerEntity currentPlayer = playerRepository.findPlayerByName( name );
                  List<PlayerEntity> playersEntity2 = new ArrayList<>();
-                 double radius = 10;
+                 double radius = 0.001; // it's about worth up 111 meters http://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude
                  double center_x = currentPlayer.getLatitude();
                  double center_y = currentPlayer.getLongitude();
                  
+                 
                  for ( int i = 0; i < playersEntity.size(); i++ )
                  {
+                     
                      if ( isInsideOfRadius( center_x, center_y, playersEntity.get(i).getLatitude(), playersEntity.get(i).getLongitude(), radius ) )
                      {
                          playersEntity2.add( playersEntity.get(i));
@@ -63,7 +65,7 @@ public class PlayerService {
 //            tmpX = Math.pow( tmpX, 2 );
 //            tmpY = y - center_y;
 //            tmpY = Math.pow( tmpY, 2 );
-//            radius = Math.pow( radius, 2 );            
+//            radius = Math.pow( radius, 2 ); 
             if ( Math.pow( x - center_x, 2 ) + Math.pow( y - center_y, 2 ) < Math.pow( radius, 2 ) )
             {
                 return true;
