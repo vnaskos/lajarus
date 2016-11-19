9af520ff7e18220eaaa5652385a77c3c5cb6c6f2
DreamerVisioner <mitrousis@outlook.com.gr>
2016-11-19 18:46:51 +0200

    creation of register activity

--- android-client/app/src/main/java/com/bugbusters/lajarus/second.java
+++ android-client/app/src/main/java/com/bugbusters/lajarus/second.java
@@ -1,13 +1,29 @@
 package com.bugbusters.lajarus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class second extends AppCompatActivity {
+
+    public Button but1;
+
+    public void init4(){
+        but1 = (Button)findViewById(R.id.button10);
+        but1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent toy = new Intent(second.this,register.class);
+                startActivity(toy);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        init4();
     }
 }
