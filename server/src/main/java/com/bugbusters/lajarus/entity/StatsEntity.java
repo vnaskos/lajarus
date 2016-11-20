ba7f921546e5c79ca22b8d848fb3b46fd5956f76
Vavouraz <xarisvavouras@gmail.com>
2016-11-20 19:11:30 +0200

    Add Player Stats

---
+++ server/src/main/java/com/bugbusters/lajarus/entity/StatsEntity.java
@@ -0,0 +1,106 @@
+package com.bugbusters.lajarus.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "stats")
+public class StatsEntity {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "level", nullable = false)
+    private long level;
+    
+    @Column(name = "health", nullable = false)
+    private String health;
+    
+    @Column(name = "power", nullable = false)
+    private double power;
+    
+    @Column(name = "defence", nullable = false)
+    private double defence;
+    
+    @Column(name = "speed", nullable = false)
+    private String speed;
+    
+    public StatsEntity() {
+        
+    }
+
+    /**
+     * @return the level
+     */
+    public long getLevel() {
+        return level;
+    }
+
+    /**
+     * @param level the level to set
+     */
+    public void setLevel(long level) {
+        this.level = level;
+    }
+
+    /**
+     * @return the health
+     */
+    public String getHealth() {
+        return health;
+    }
+
+    /**
+     * @param health the health to set
+     */
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    /**
+     * @return the power
+     */
+    public double getPower() {
+        return power;
+    }
+
+    /**
+     * @param power the power to set
+     */
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    /**
+     * @return the defense
+     */
+    public double getDefence() {
+        return defence;
+    }
+
+    /**
+     * @param defence the defense to set
+     */
+    public void setDefence(double defence) {
+        this.defence = defence;
+    }
+
+    /**
+     * @return the speed
+     */
+    public String getSpeed() {
+        return speed;
+    }
+
+    /**
+     * @param speed the speed to set
+     */
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+    
+    
+}
+
