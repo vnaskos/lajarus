8dfe10f84c6260113475f7a0d6067e22a640fdb6
thanasismavridis <thanasis_mavridis@yahoo.gr>
2016-12-06 18:19:13 +0200

    Merge branch 'master' of C:\Users\thanasis\AndroidStudioProjects\lajarus with conflicts.

--- android-client/app/src/main/java/com/bugbusters/lajarus/LoginActivity.java
+++ android-client/app/src/main/java/com/bugbusters/lajarus/LoginActivity.java
@@ -1,12 +1,13 @@
 package com.bugbusters.lajarus;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bugbusters.lajarus.entity.Session;
@@ -15,6 +16,7 @@ import com.bugbusters.lajarus.manager.TokenManager;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView t;
 
     private TokenManager tokenManager;
 
@@ -27,6 +29,30 @@ public class LoginActivity extends AppCompatActivity {
 
         attachLoginBtnListener();
         attachRegisterBtnListener();
+
+
+        t= (TextView) findViewById(R.id.loginButton);
+        Typeface myCustomFont= Typeface.createFromAsset(getAssets(), "fonts/trajanus.ttf");
+        t.setTypeface(myCustomFont);
+
+        t= (TextView) findViewById(R.id.registerButton);
+        Typeface myCustomFont3= Typeface.createFromAsset(getAssets(), "fonts/trajanus.ttf");
+        t.setTypeface(myCustomFont3);
+
+        t= (TextView) findViewById(R.id.usernameField);
+        Typeface myCustomFont1= Typeface.createFromAsset(getAssets(), "fonts/trajanus.ttf");
+        t.setTypeface(myCustomFont1);
+
+        t= (TextView) findViewById(R.id.passwordField);
+        Typeface myCustomFont2= Typeface.createFromAsset(getAssets(), "fonts/trajanus.ttf");
+        t.setTypeface(myCustomFont2);
+
+        t= (TextView) findViewById(R.id.loginLabel);
+        Typeface myCustomFont4= Typeface.createFromAsset(getAssets(), "fonts/trajanus.ttf");
+        t.setTypeface(myCustomFont4);
+
+
+
     }
 
     public void attachLoginBtnListener() {
