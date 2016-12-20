a5de587b0e540c53ccb01d6662417d9c35aab401
Karen <nersesiankaren@gmail.com>
2016-12-20 16:53:10 +0200

    Modyfying the quest Repository

--- server/src/main/java/com/bugbusters/lajarus/repository/QuestRepository.java
+++ server/src/main/java/com/bugbusters/lajarus/repository/QuestRepository.java
@@ -10,11 +10,27 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 
+/*
+    An interface that acces directly to the Database via the entities wit hthe
+    support of spring framewokr. The methods of this class are communicating
+    with the services and the databases.
+*/
 @Repository
 public interface QuestRepository extends JpaRepository<QuestEntity, Long> {
-
+    
+    
     public QuestEntity getQuestById( @Param("id") long id );
     
+    /*
+        The @Query Annotation is from spring framework witch help us to acces
+        directly to the database via SQL Query. The mathematical type is calculate
+        and returning a list of quests that are near to the player. The informations
+        are returning to the Service methods.
+        
+        The @Param Annotation "telling us that the field with @fieldName(parameter of
+        @Param Annotation) is going to be descriped with a variable @fieldName 
+        (parameter of the method)
+    */
     @Query("SELECT q FROM QuestEntity q"
             + " WHERE ( 6371 * acos( cos( radians( :latitude ) )"
             + " * cos( radians( q.latitude ) )"
@@ -25,7 +41,13 @@ public interface QuestRepository extends JpaRepository<QuestEntity, Long> {
             @Param("latitude") double latatitude,
             @Param("longitude") double longitude,
             @Param("distance") double radius);
-    
+    /*
+        @Transactional Annotation is from spring framework that give us rights
+        to the Database.
+        @Modidying Annotation is from spring framweork that give us rights
+        to modify th field of the database.
+        In this Query we delete a quest with a speicific id.
+    */
     @Transactional
     @Modifying
     @Query( "DELETE from QuestEntity q WHERE q.id = :id" )
