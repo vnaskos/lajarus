a0c1e383b375c175a5a9e107e260bd323de4f2cf
Karen <nersesiankaren@gmail.com>
2016-12-20 18:27:36 +0200

    Modyfying the PlayerRepository NOT critical changes.

--- server/src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
+++ server/src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
@@ -10,11 +10,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 
-/*
-    An interface that acces directly to the Database via the entities wit hthe
-    support of spring framewokr. The methods of this class are communicating
-    with the services and the databases.
-*/
+
 /*
     An interface that acces directly to the Database via the entities wit hthe
     support of spring framewokr. The methods of this class are communicating
@@ -28,16 +24,7 @@ public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
     
     public PlayerEntity findPlayerByName(@Param("name") String name);
     
-     /*
-        The @Query Annotation is from spring framework witch help us to acces
-        directly to the database via SQL Query. The mathematical type is calculate
-        and returning a list of quests that are near to the player. The informations
-        are returning to the Service methods.
-        
-        The @Param Annotation "telling us that the field with @fieldName(parameter of
-        @Param Annotation) is going to be descriped with a variable @fieldName 
-        (parameter of the method)
-    */
+    
     /*
         The @Query Annotation is from spring framework witch help us to acces
         directly to the database via SQL Query. The mathematical type is calculate
@@ -60,13 +47,7 @@ public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
             @Param("longitude") double longitude,
             @Param("distance") double radius);
     
-    /*
-        @Transactional Annotation is from spring framework that give us rights
-        to the Database.
-        @Modidying Annotation is from spring framweork that give us rights
-        to modify th field of the database.
-        In this Query we delete a quest with a speicific id.
-    */
+    
     /*
         @Transactional Annotation is from spring framework that give us rights
         to the Database.
