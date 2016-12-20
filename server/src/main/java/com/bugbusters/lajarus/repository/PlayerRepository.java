d03b40f0ee530ba09f5b0c25ac120dc688724edc
Karen <nersesiankaren@gmail.com>
2016-12-20 18:26:16 +0200

    Modyfying the PlayerRepository NOT critical changes.

--- server/src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
+++ server/src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
@@ -15,6 +15,11 @@ import org.springframework.data.jpa.repository.Modifying;
     support of spring framewokr. The methods of this class are communicating
     with the services and the databases.
 */
+/*
+    An interface that acces directly to the Database via the entities wit hthe
+    support of spring framewokr. The methods of this class are communicating
+    with the services and the databases.
+*/
 
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
@@ -24,6 +29,16 @@ public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
     public PlayerEntity findPlayerByName(@Param("name") String name);
     
      /*
+        The @Query Annotation is from spring framework witch help us to acces
+        directly to the database via SQL Query. The mathematical type is calculate
+        and returning a list of quests that are near to the player. The informations
+        are returning to the Service methods.
+        
+        The @Param Annotation "telling us that the field with @fieldName(parameter of
+        @Param Annotation) is going to be descriped with a variable @fieldName 
+        (parameter of the method)
+    */
+    /*
         The @Query Annotation is from spring framework witch help us to acces
         directly to the database via SQL Query. The mathematical type is calculate
         and returning a list of quests that are near to the player. The informations
@@ -45,6 +60,13 @@ public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
             @Param("longitude") double longitude,
             @Param("distance") double radius);
     
+    /*
+        @Transactional Annotation is from spring framework that give us rights
+        to the Database.
+        @Modidying Annotation is from spring framweork that give us rights
+        to modify th field of the database.
+        In this Query we delete a quest with a speicific id.
+    */
     /*
         @Transactional Annotation is from spring framework that give us rights
         to the Database.
