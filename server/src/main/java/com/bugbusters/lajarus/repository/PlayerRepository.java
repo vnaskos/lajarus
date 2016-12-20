ce8a8ee5bbc38c7b4b3daf8136d862fbc94373a7
Karen <Karen@Karen>
2016-12-20 18:25:16 +0200

    Modyfying the PlayerRepository NOT critical changes.

--- server/src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
+++ server/src/main/java/com/bugbusters/lajarus/repository/PlayerRepository.java
@@ -10,12 +10,29 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 
+/*
+    An interface that acces directly to the Database via the entities wit hthe
+    support of spring framewokr. The methods of this class are communicating
+    with the services and the databases.
+*/
+
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 
     public PlayerEntity findPlayerById(long id);
     
     public PlayerEntity findPlayerByName(@Param("name") String name);
+    
+     /*
+        The @Query Annotation is from spring framework witch help us to acces
+        directly to the database via SQL Query. The mathematical type is calculate
+        and returning a list of quests that are near to the player. The informations
+        are returning to the Service methods.
+        
+        The @Param Annotation "telling us that the field with @fieldName(parameter of
+        @Param Annotation) is going to be descriped with a variable @fieldName 
+        (parameter of the method)
+    */
     
     @Query("SELECT p FROM PlayerEntity p"
             + " WHERE ( 6371 * acos( cos( radians( :latitude ) )"
@@ -27,6 +44,14 @@ public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
             @Param("latitude") double latatitude,
             @Param("longitude") double longitude,
             @Param("distance") double radius);
+    
+    /*
+        @Transactional Annotation is from spring framework that give us rights
+        to the Database.
+        @Modidying Annotation is from spring framweork that give us rights
+        to modify th field of the database.
+        In this Query we delete a quest with a speicific id.
+    */
     
     @Transactional
     @Modifying(clearAutomatically = true)
