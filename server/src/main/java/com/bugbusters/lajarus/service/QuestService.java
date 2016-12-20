9ca01156b8fe0e8d7bd9785170d0d326d6f8e496
Karen <Karen@Karen>
2016-12-20 16:30:08 +0200

    Modyfying the quest Service

--- server/src/main/java/com/bugbusters/lajarus/service/QuestService.java
+++ server/src/main/java/com/bugbusters/lajarus/service/QuestService.java
@@ -10,15 +10,26 @@ import com.bugbusters.lajarus.entity.QuestEntity;
 import com.bugbusters.lajarus.repository.QuestRepository;
 import com.bugbusters.lajarus.repository.PlayerRepository;
 
+/*
+    Type: Service
+    This Service is the midlman between the QuestController and the QuestEntity
+    with is conected with the Database with the support of the spring framework.
+    This is receiving the informations from the controller and modifying it for the
+    needs of the client applications.
+*/
+
 @Service
 public class QuestService {
-
+    //An object of QuestRepository Interface that acces directly to the database
     @Autowired
     private QuestRepository questRepository;
     
+    //An object of PlayerRepository Interface that acces directly to the database
     @Autowired
     private PlayerRepository playerRepository;
-
+    
+    //This method of the service is returning the quest with a specific @param id
+    //Also it is checking if the data that would be returned is NULL or not
     public QuestEntity getQuestById(long id) throws Exception {
         QuestEntity questEntity = questRepository.getQuestById(id);
         
@@ -28,19 +39,27 @@ public class QuestService {
 
         return questEntity;
     }
-
+    
+    
     public List<QuestEntity> getAll() {
         return questRepository.findAll();
     }
-
+    
+    //Method of service that creats a new quest on Database
     public void createQuest(QuestEntity questEntity) {
         questRepository.saveAndFlush(questEntity);
     }
-
+    
+    //Deleting the quest wit ha specific @param id
     public void deleteQuest(long id) {
         questRepository.deleteQuest(id);
     }
     
+    /*
+        A list of quests that are returning to the controller for the quests
+        that are near to the player with a specific @param name (the name of the players)
+        with the help of geolocation
+    */
     public List<QuestEntity> getNearbyQuests(String name) {
         PlayerEntity player = playerRepository.findPlayerByName(name);
         List<QuestEntity> nearbyQuests = questRepository.findNearByQuests(
@@ -48,6 +67,10 @@ public class QuestService {
         return nearbyQuests;
     }
     
+    /* 
+        A service method that check if there is a quest with a specific @param name
+        and returing true or false
+    */
     public boolean isThereQuestByName( String name )
     {
         QuestEntity questEntity = questRepository.isThereQuestByName( name );
