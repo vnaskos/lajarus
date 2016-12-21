0c7f91e9bd76b650757a8e4efe2c9e08d0a80bb3
milenazhuleku@gmail.com <>
2016-12-21 11:52:27 +0200

    ImageUtil,resizedImage

---
+++ server/src/main/java/com/bugbusters/lajarus/ImageUtil/resizedImage.java
@@ -0,0 +1,53 @@
+
+package com.bugbusters.lajarus.ImageUtil;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+public class resizedImage {
+    
+    
+    /**
+     * Resizes an image to a absolute width and height (the image may not be
+     * proportional)
+     *
+     * @param inputImagePath Path of the original image  
+     * @param outputImagePath Path to save the resized image
+     * @param scaledWidth absolute width in pixels
+     * @param scaledHeight absolute height in pixels
+     * @throws IOException
+     */
+    private static final int IMG_WIDTH = 70;
+    private static final int IMG_HEIGHT = 70;
+    
+  
+    public static void resize(String inputImagePath,
+           
+            String outputImagePath, int scaledWidth, int scaledHeight)
+            throws IOException {
+        // reads input image
+        File inputFile = new File(inputImagePath);
+        BufferedImage inputImage = ImageIO.read(inputFile);
+
+        // creates output image
+        BufferedImage outputImage = new BufferedImage(scaledWidth,
+                scaledHeight, inputImage.getType());
+
+        // scales the input image to the output image
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+        g2d.dispose();
+
+        // extracts extension of output file
+        String formatName = outputImagePath.substring(outputImagePath
+                .lastIndexOf(".") + 1);
+
+        // writes to output file
+        ImageIO.write(outputImage, formatName, new File(outputImagePath));
+    }
+   
+}
+    
+
